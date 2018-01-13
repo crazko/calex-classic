71a33459cfb9f08d62898ec696ae369f81123ce7
Roman Vesely <crazko@gmail.com>
2018-01-13 14:41:27 +0100

    updated release script to support mac
    https://stackoverflow.com/questions/7573368/in-place-edits-with-sed-on-os-x

--- release.sh
+++ release.sh
@@ -18,15 +18,15 @@ if [ ! -d $DESTINATION_DIR ]; then
 fi
 
 # update version number
-sed -i '5s/.*/  "version": "'$VERSION'",/g' manifest.json
-sed -i "s/Unreleased/[$VERSION] - $DATE/g" CHANGELOG.md
+sed -i '' '5s/.*/  "version": "'$VERSION'",/g' manifest.json
+sed -i '' "s/Unreleased/[$VERSION] - $DATE/g" CHANGELOG.md
 
 # copy release files
 cp -R _locales assets icons manifest.json $DESTINATION_DIR
 
 # update api keys
-sed -i "s/$CLIENT_ID_DEV/$CLIENT_ID_PROD/g" $DESTINATION_DIR/manifest.json
-sed -i "s/$CLIENT_SECRET_DEV/$CLIENT_SECRET_PROD/g" $DESTINATION_DIR/assets/js/background.js
+sed -i '' "s/$CLIENT_ID_DEV/$CLIENT_ID_PROD/g" $DESTINATION_DIR/manifest.json
+sed -i '' "s/$CLIENT_SECRET_DEV/$CLIENT_SECRET_PROD/g" $DESTINATION_DIR/assets/js/background.js
 
 #remove unnecessary "key" key
-sed -i '/key/d' $DESTINATION_DIR/manifest.json
+sed -i '' '/key/d' $DESTINATION_DIR/manifest.json
